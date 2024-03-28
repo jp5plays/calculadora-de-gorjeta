b695a677624a6f0afc209c6ebaa9b5186416e414
João Pedro <ribeirobritojoaopedro@gmail.com>
2024-03-28 19:24:29 -0300

    sexto commit do js e agora sim projeto FINALIZADO

--- script.js
+++ script.js
@@ -74,7 +74,11 @@ function limpar(){
     })
     gorjetaInput.value = ""
     pessoasInput.value = ""
-    document.querySelector(".gorjeta-total>strong").innerHTML="R% 0.00"
+    document.querySelector(".gorjeta-total>strong").innerHTML="R$ 0.00"
     document.querySelector(".total > strong").innerHTML = "R$ 0.00"
+
+    conta=0
+    porcentagem=0
+    pessoas=0
 
 }
